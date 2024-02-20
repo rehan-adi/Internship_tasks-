93568a792cee9eadbd0f435a85e8b391403f677d
Rehan <rehanalire52@gmail.com>
2024-02-20 18:46:55 +0530

    Learning about super, extends and instance and many more things

--- oops.js
+++ oops.js
@@ -14,3 +14,17 @@ const Tesla = new Car('Tesla X', '3.2 cr', '10')
 console.log(Tesla.brand);
 const Audi = new Car('Auid R8', '2 Cr', 'R8');
 console.log(Audi);
+
+class Plane extends Car{
+    constructor(brand, price, model, capacity){
+          super(brand, price, model)
+          this.capacity = capacity;
+    }
+}
+
+const AirIndia =  new Plane ('Air India', '400M', 'AirBus A7', 5000)
+const FedEx =  new Plane ('FedEx', '300M', 'Boing 400 A7', 3000)
+const Indigo =  new Plane ('Indigo', '480M', 'AirBus 747', 3200)
+
+console.log(AirIndia.brand);
+console.log(AirIndia.model);
