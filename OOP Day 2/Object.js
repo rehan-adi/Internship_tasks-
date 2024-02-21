9370b7ca21f6e017fb8fa9f8ac09cfb0e5630df7
Rehan <rehanalire52@gmail.com>
2024-02-21 17:28:51 +0530

    Learning about de - structuring, nasted object and mearge two objects

--- OOP Day 2/Object.js
+++ OOP Day 2/Object.js
@@ -3,6 +3,13 @@ const WebMatrixCoders = [{
     frameworks: ['React', 'Node.js', 'Next Js'],
     isHiring: true,
     remoteWork: true,
+    nastedTwo: {
+        nastedOne: {
+            nasted: {
+                name: 'Rehan',
+            }
+        }
+    }
 }, 
 {
         experienceLevel: 'Expert',
@@ -13,18 +20,44 @@ const WebMatrixCoders = [{
 },
 ];
 
+// de - structuring
+const {bestCoders: Coders} = WebMatrixCoders[0]
+console.log(Coders);
 
-console.log(WebMatrixCoders[0].bestCoders);
 
 WebMatrixCoders[1].tool.forEach((data) => {
     console.log(data);
-})
+});
+
+console.log(WebMatrixCoders[0].nastedTwo.nastedOne.nasted.name);
 
 
 WebMatrixCoders[0].frameworks.forEach((data) => {
      console.log(data);
 }) 
 
+const {teamSize: sizeOTeam} = WebMatrixCoders[1];
+console.log(sizeOTeam);
+
 WebMatrixCoders[1].experienceLevel = 'Top';
 console.log(WebMatrixCoders[1].experienceLevel);
 console.log(WebMatrixCoders);
+
+
+const user1 = {
+    Profession: 'Developer',
+    marvelFan: true,
+    email: 'IronMan@avenger.com',
+}
+
+const user2 = {
+    name: 'Tony Stark',
+    age: 2000,
+}
+
+const user3 = {...user1, ...user2};
+console.log(user3);
+console.log(Object.keys(user1));
+console.log(Object.values(user1));
+console.log(Object.entries(user1));
+console.log(Object.hasOwnProperty('Thor'));
