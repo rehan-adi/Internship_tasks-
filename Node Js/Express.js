5791bdac87b7cfdea2cf5fb51edf9b4aa1110c23
Rehan <rehanalire52@gmail.com>
2024-02-24 16:11:37 +0530

    Learning about Express js

---
+++ Node Js/Express.js
@@ -0,0 +1,27 @@
+// ES6 Module
+import Express from "express";
+// Common js
+// const fs = require('fs')
+
+const expressPower = Express();
+const port = 3333;
+
+expressPower.get('/', (req, res) => {
+     res.send('Hellowww from express server')
+}).get('/about', (req, res) => {
+     res.send('This is about page')
+})
+
+
+expressPower.get('/user', (req, res) => {
+    res.send('This is user page')
+})
+
+expressPower.get('/user:id', (req, res) => {
+    const data = req.params.id;
+    res.send(`Hi ${data}, how are you !`)
+})
+
+expressPower.listen(port, () => {
+     console.log(`Server is running localhost:${port} `);
+})
