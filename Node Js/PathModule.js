bd33fa89830aba3f0fac79c4c52dddf21978c506
Rehan <rehanalire52@gmail.com>
2024-02-24 15:57:53 +0530

    Learning about fs and os module

---
+++ Node Js/PathModule.js
@@ -0,0 +1,6 @@
+import path from 'path'
+
+
+console.log(path.dirname('C:\Users\Rehan Ali\OneDrive\Desktop\Web Matrix\JavaScript'));
+console.log(path.basename('C:\Users\Rehan Ali\OneDrive\Desktop\Web Matrix\JavaScript'));
+console.log(path.join('C:\Users\Rehan Ali\OneDrive\Desktop\Web Matrix\JavaScript'));
